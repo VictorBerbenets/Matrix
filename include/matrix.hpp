7e4a45880cc90057494c8a0c702c31f5d76403b3
Victor Berbenets <berbenets.vd@phystech.edu>
2023-09-17 14:27:34 +0300

    just making starting routine

---
+++ include/matrix.hpp
@@ -0,0 +1,23 @@
+#ifndef MATRIX_
+#define MATRIX_
+
+namespace yLAB {
+
+template<typename T, std::size_t N>
+class Matrix {
+    using value_type       = T;
+    using pointer          = T*;
+    using reference        = T&;
+    using const_value_type = const value_type;
+    using const_pointer    = const pointer;
+    using const_reference  = const reference;
+public:
+
+
+private:
+};
+
+}
+
+#endif
+
